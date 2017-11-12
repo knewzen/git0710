f18a149a158c585ac25389157a2f71bf952cb684
rw <405212760@qq.com>
2017-11-12 23:23:42 +0800

    00

--- 三种开发模式/VUE/01/code/1.单页应用及vue原理封装演示/05form-value.js
+++
@@ -1,9 +0,0 @@
-function DataBind(options) {
-    var obj = options.data();
-    var oInputs = document.querySelectorAll('[v-model]');
-    for (var i = 0; i < oInputs.length; i++) {
-        var oInput = oInputs[i];
-        var attrVal = oInput.getAttribute('v-model');
-        oInput.value = obj[attrVal];
-    }
-}
